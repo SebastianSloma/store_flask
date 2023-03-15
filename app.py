d1e53a76340e1da81262f951d2f805babd642e5e
Sebastian <sebastiansloma@gmail.com>
2023-03-15 18:50:36 +0100

    .

--- app.py
+++ app.py
@@ -4,6 +4,24 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SECRET_KEY'] = "mySecretK3y"
+
+db = SQLAlchemy(app)
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024),
+                            nullable=False, unique=True)
+    
+
+def __repr__(self):
+        return f'Item {self.name}'
+
 
 @app.route('/')
 @app.route('/home')
