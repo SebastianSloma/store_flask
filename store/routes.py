09466eb4ee3a2816d48072cbc044ca70ca426e1d
Sebastian <sebastiansloma@gmail.com>
2023-03-17 12:31:17 +0100

    login form

--- store/routes.py
+++ store/routes.py
@@ -1,7 +1,8 @@
+from flask_login import login_user
 from store import app
 from flask import render_template, redirect, url_for, flash
 from store.models import Item, User
-from store.forms import RegisterForm
+from store.forms import RegisterForm, LoginForm
 from store import db
 
 
@@ -37,4 +38,14 @@ def register_page():
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
     form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(
+                attempted_password=form.password.data
+        ):
+            login_user(attempted_user)
+            flash(f'Success! You are logged in as: {attempted_user.username}', category='success')
+            return redirect(url_for('market_page'))
+        else:
+            flash('Username and password are not match! Please try again', category='danger')
     return render_template('login.html', form=form)
